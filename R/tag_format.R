58c0147de18313f5ad62acb202efa79770678eaa
Anders Kagervall <anders.kagervall@gmail.com>
2023-05-19 12:53:38 +0200

    Remove old commented code

--- R/tag_format.R
+++ R/tag_format.R
@@ -32,15 +32,3 @@ hex2dec <- function(x) {
   return(r)
 }
 
-## toHex  <- function(x) {
-##     bs <- as.bitstring(as.integer64(x))
-##     bits  <- unlist(strsplit(bs, NULL))
-##     res  <- rep(NA, 8)
-##     for (i in 1:8) {
-##         last  <- i * 8
-##         first  <- last - 7
-##         res[i] <- paste0(bits[first:last], collapse = "")
-##     }
-##     res <- paste0(sprintf("%X", strtoi(res[4:8], base= 2)), collapse = "")
-##     return(res)
-## }
